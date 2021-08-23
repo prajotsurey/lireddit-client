fe0d5953739a51b15bb0bbca4e64c5b30290ff03
prajotsurey <prajotsurey@gmail.com>
2021-08-23 19:33:12 +0530

    set up change password page

---
+++ src/pages/change-password/[token].tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+
+export const ChangePassword: NextPage<{token: string} > = ({token}) => {
+    return (
+      <div>
+        token is {token}
+      </div>
+    );
+}
+
+
+
+ChangePassword.getInitialProps = ({query}) => {
+  return {
+    token: query.token as string
+  }
+}
+
+export default ChangePassword
