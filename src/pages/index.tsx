7fd9e146095747b27f97b0735f78c57546241af8
prajotsurey <prajotsurey@gmail.com>
2021-08-25 18:10:02 +0530

    added chakra ui "stack" element to display posts updated posts query to include text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
+import NextLink from 'next/link';
 import React from "react";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import NextLink from 'next/link';
-import { Link } from "@chakra-ui/react";
 
 const Index = () => {
   const [{data}] = usePostsQuery({
@@ -19,8 +19,18 @@ const Index = () => {
          create post
         </Link>
       </NextLink>
-      <div>Hello world</div>
-      {!data ? <div>loading...</div> : data.posts.map( p => <div key={p.id}>{p.title}</div>)}
+      {!data ? (
+      <div>loading...</div>
+      ) : (
+        <Stack spacing={8}>
+          { data.posts.map( p => (
+            <Box p={5} key={p.id} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl">{p.title}</Heading>
+            <Text mt={4}>{p.text}</Text>
+            </Box>
+          )) }
+        </Stack>
+      )}
     </Layout>
   )
 }
