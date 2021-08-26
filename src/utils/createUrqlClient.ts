48a55d43d0cd99b69d386e59760992e1701d4dd7
prajotsurey <prajotsurey@gmail.com>
2021-08-26 20:45:26 +0530

    added cache invalidation fo r posts query

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -72,6 +72,18 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          createPost: (_result, args, cache, info) => {
+            const allFields = cache.inspectFields('Query');
+            const fieldInfos = allFields.filter(info => info.fieldName === 'posts');
+            fieldInfos.forEach( (fi) => {
+              cache.invalidate("Query", "posts", fi.arguments || {});
+            })
+            cache.invalidate('Query', 'posts', {
+              variables: {
+                limit: 15,
+              }
+            })
+          },
           logout: (_result, args, cache, info) => {
             betterUpdateQuery<LogoutMutation , MeQuery>(
               cache,
